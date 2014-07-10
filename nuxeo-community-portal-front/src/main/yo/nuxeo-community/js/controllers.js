a965c3290cbda2c79e9da89bf92942844e14c0ad
Geoffrey Gautier <ggautier@nuxeo.com>
2014-07-10 17:30:24 +0200

    Mise à jour 0.3
    Changement affichage activités

--- nuxeo-community-portal-front/src/main/yo/nuxeo-community/js/controllers.js
+++ nuxeo-community-portal-front/src/main/yo/nuxeo-community/js/controllers.js
@@ -5,7 +5,7 @@
 var app = angular.module("app", []);
 
 var client = new nuxeo.Client({
-  baseURL: 'http://demo.nuxeo.com/nuxeo',
+  baseURL: 'http://localhost:8080/nuxeo/',
   username: 'Administrator',
   password: 'Administrator'
 });
@@ -16,6 +16,13 @@ client.connect(function(error, client) {
     throw new Error(error);
   }
 });
+
+client.schemas(['dublincore','common', 'file']);
+
+// client.request("path//default-domain/workspaces/Activities").get(function (error, data, response){ 
+//     doc=data;
+//     console.log(doc);
+//   });
 
 app.controller("userListController", function ($scope) {
   $scope.users = [
@@ -38,66 +45,32 @@ app.controller("userListController", function ($scope) {
      "description" : "Né le 68 Février 2798 . Il travaille chez Solucom depuis 7 ans.Travaille actuellement en tant que Directeur commercial. A étudier à Central Paris. Aime le football, la natation, les films dhorreur et le ketchup" ,
      "info" : "Alain a gagné la médaille POPO il y a 3 jours"}
   ];
-});
+}); 
 
-app.controller("activityListController", function ($scope) {
-  $scope.activities = [
-    {"title": "Pif",
-    "id":"0",
-    "type": "Answers",
-    "author" : "Bill",
-    "date": "Utlisateur depuis le 13/06/13",
-    "comments": [
-     {"author": "George Alain",
-      "pic": "http://www.bobmarleymusic.us/Pictures/images/albums/big/Bob_Marley/2008_-_Bob_Marley_Vs_Lee_Scratch_Perry-The_Best_Of_The_Upsetter_Years.jpg",
-      "text": "Nice post DD"
-      },
-     {"author": "Michel Berger",
-      "pic": "http://www.bobmarleymusic.us/Pictures/images/albums/big/Bob_Marley/2008_-_Bob_Marley_Vs_Lee_Scratch_Perry-The_Best_Of_The_Upsetter_Years.jpg",
-      "text": "Well done"
-       },
-     {"author": "George Alain",
-      "pic": "http://www.bobmarleymusic.us/Pictures/images/albums/big/Bob_Marley/2008_-_Bob_Marley_Vs_Lee_Scratch_Perry-The_Best_Of_The_Upsetter_Years.jpg",
-      "text": "Amazing"
-     }]
-    },
-     {"title": "Pif",
-     "id":"1",
-     "type": "Answers",
-     "author" : "Administrator",
-     "date": "Utlisateur depuis le 13/06/13",
-     "comments": [
-     {"author": "George Alain",
-      "pic": "http://www.bobmarleymusic.us/Pictures/images/albums/big/Bob_Marley/2008_-_Bob_Marley_Vs_Lee_Scratch_Perry-The_Best_Of_The_Upsetter_Years.jpg",
-      "text": "Nice post DD"
-     }]},
-     {"title": "Pif",
-     "id":"2",
-     "type": "Answers",
-     "author" : "Ornella",
-     "date": "Utlisateur depuis le 13/06/13",
-     "comments": [
-     {"author": "George Alain",
-      "pic": "http://www.bobmarleymusic.us/Pictures/images/albums/big/Bob_Marley/2008_-_Bob_Marley_Vs_Lee_Scratch_Perry-The_Best_Of_The_Upsetter_Years.jpg",
-      "text": "Nice post DD"
-     }]},
-     {"title": "Pif",
-     "type": "Answers",
-     "author" : "George Alain",
-     "date": "Utlisateur depuis le 13/06/13"
-     },
-     {"title": "Pif",
-     "type": "Answers",
-     "author" : "Charles H.",
-     "date": "Utlisateur depuis le 13/06/13"
-     },
-     {"title": "Pif",
-     "type": "Answers",
-     "author" : "Charles H.",
-     "date": "Utlisateur depuis le 13/06/13"
-     }
-  ];
-});
+// app.controller('activityListController', function ($rootScope) {
+//   client.operation('Document.Query')
+//   .input('SELECT (*) FROM ActivityCommunity')
+//   .execute(function(error, data) {
+//     if (error) {
+//       // something went wrong
+//       throw error;
+//     }
+//   console.log(data)
+//   })
+// }); 
+
+app.controller('activityListController', function ($rootScope) {
+  client.operation('Document.GetChildren')
+  .input('/default-domain/workspaces/Activities')
+  .execute(function(error, children) {
+    if (error) {
+      // something went wrong
+      throw error;
+    }
+    $rootScope.activities = children;
+    $rootScope.$apply();
+  })
+}); 
 
 app.controller('commentsController', ['$scope', function($scope) { 
     $scope.showComments = function(id) {
@@ -110,6 +83,7 @@ app.controller('userProfilController', ['$rootScope', function($rootScope) {
       client.request("/user/"+username).get(function (error,user){
       $rootScope.user=user;
       $rootScope.$apply();
+      console.log(user)
       $("#allUsers").modal('hide.modal');   
       $("#profil").modal('show');
       });
